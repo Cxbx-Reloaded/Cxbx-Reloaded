a21e652da54706eb0efd7914e75dee739b12be67
Aaron Robinson <caustik@caustik.com>
2006-07-15 02:19:18 +0000

    - fixed export script (added debug with pdb and all)

--- build/win32/Export.bat
+++ build/win32/Export.bat
@@ -1,7 +1,12 @@
-cd ..\..\Bin\release
-..\Import\upx\bin\upx -9 Cxbx.exe
-..\Import\upx\bin\upx -9 Cxbx.dll
-copy Cxbx.exe ..\Export\Win32\Bin\
-copy Cxbx.dll ..\Export\Win32\Bin\
-cd ..\Build\win32
+cd ..\..\bin\release
+..\..\import\upx\bin\upx -9 Cxbx.exe
+..\..\import\upx\bin\upx -9 Cxbx.dll
+copy Cxbx.exe ..\..\export\win32\bin\release\
+copy Cxbx.dll ..\..\export\win32\bin\release\
+cd ..\..\bin\debug
+copy Cxbx.exe ..\..\export\win32\bin\debug\
+copy Cxbx.pdb ..\..\export\win32\bin\debug\
+copy CxbxKrnl.dll ..\..\export\win32\bin\debug\
+copy CxbxKrnl.pdb ..\..\export\win32\bin\debug\
+cd ..\..\build\win32
 
