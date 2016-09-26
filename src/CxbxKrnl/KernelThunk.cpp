aa2167ccb94aef9e7f9eed6c228e941f1109edfe
Luke Usher <luke.usher@outlook.com>
2016-09-26 22:02:10 +0100

    Oops

--- src/CxbxKrnl/KernelThunk.cpp
+++ src/CxbxKrnl/KernelThunk.cpp
@@ -261,7 +261,7 @@ extern "C" CXBXKRNL_API uint32 CxbxKrnl_KernelThunkTable[367] =
     (uint32)PANIC(0x00C9),                          // 0x00C9 (201) NtOpenDirectoryObject
     (uint32)&xboxkrnl::NtOpenFile,                  // 0x00CA (202)
     (uint32)&xboxkrnl::NtOpenSymbolicLinkObject,    // 0x00CB (203) NtOpenSymbolicLinkObject
-    (uint32)PANIC(0x00CC),                          // 0x00CC (204) NtProtectVirtualMemory
+    (uint32)&xboxkrnl::NtProtectVirtualMemory,      // 0x00CC (204) NtProtectVirtualMemory
     (uint32)PANIC(0x00CD),                          // 0x00CD (205) NtPulseEvent
     (uint32)&xboxkrnl::NtQueueApcThread,            // 0x00CE (206)
     (uint32)&xboxkrnl::NtQueryDirectoryFile,        // 0x00CF (207)
