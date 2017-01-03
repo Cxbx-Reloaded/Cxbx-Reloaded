9ad4449ef9a7cf98695e73075a38db421288919a
PatrickvL <pvanlogchem@gmail.com>
2017-01-03 12:17:20 +0100

    Emulated EEPROM read/write
    Implemented ExQueryNonVolatileSetting and ExSaveNonVolatileSetting using
    emulated EEPROM values.

--- import/OpenXDK/include/xboxkrnl/xboxkrnl.h
+++ import/OpenXDK/include/xboxkrnl/xboxkrnl.h
@@ -169,6 +169,23 @@ typedef long                            NTSTATUS;
 #define STATUS_OBJECT_NAME_COLLISION     ((DWORD   )0xC0000035L)
 
 // ******************************************************************
+// * Registry value types
+// ******************************************************************
+// Used in ExQueryNonVolatileSetting and ExSaveNonVolatileSetting
+#define REG_NONE                    ( 0 )   // No defined value type.
+#define REG_SZ                      ( 1 )   // A null - terminated string. This will be either a Unicode or an ANSI string, depending on whether you use the Unicode or ANSI functions.
+#define REG_EXPAND_SZ               ( 2 )   // A null - terminated string that contains unexpanded references to environment variables (for example, "%PATH%"). It will be a Unicode or ANSI string depending on whether you use the Unicode or ANSI functions. To expand the environment variable references, use the ExpandEnvironmentStrings function.
+#define REG_BINARY                  ( 3 )   // Binary data in any form.
+#define REG_DWORD                   ( 4 )   // A 32 - bit number.
+#define REG_DWORD_LITTLE_ENDIAN     ( 4 )   // A 32 - bit number in little - endian format. Windows is designed to run on little - endian computer architectures. Therefore, this value is defined as REG_DWORD in the Windows header files.
+#define REG_DWORD_BIG_ENDIAN        ( 5 )   // A 32 - bit number in big - endian format. Some UNIX systems support big - endian architectures.
+#define REG_LINK                    ( 6 )   // A null - terminated Unicode string that contains the target path of a symbolic link that was created by calling the RegCreateKeyEx function with REG_OPTION_CREATE_LINK.
+#define REG_MULTI_SZ                ( 7 )   // A sequence of null - terminated strings, terminated by an empty string(\0). String1\0String2\0String3\0LastString\0\0								 // The first \0 terminates the first string, the second to the last \0 terminates the last string, and the final \0 terminates the sequence.Note that the final terminator must be factored into the length of the string.
+#define REG_RESOURCE_LIST           ( 8 )   // Resource list in the resource map
+#define REG_FULL_RESOURCE_DESCRIPTOR ( 9 )  // Resource list in the hardware description
+#define REG_RESOURCE_REQUIREMENTS_LIST ( 10 )
+
+// ******************************************************************
 // * PAGE Masks
 // ******************************************************************
 #define PAGE_NOACCESS          0x01
