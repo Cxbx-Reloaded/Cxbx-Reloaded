Cxbx-Reloaded/Cxbx-Reloaded

commit 53a731009f7fa4cabd944033fefdb4a1b1a99939

Removed pLibraryVersion argument from EmuHLEIntercept
Since it can be reconstructed from the pXbeHeader argument

src/CxbxKrnl/CxbxKrnl.cpp
@@ -528,7 +528,7 @@ extern "C" CXBXKRNL_API void CxbxKrnlInit
 		XTL::EmuD3DInit(pXbeHeader, dwXbeHeaderSize);
 	}
 
-	EmuHLEIntercept(pLibraryVersion, pXbeHeader);
+	EmuHLEIntercept(pXbeHeader);
 
 	//
 	// initialize FS segment selector

src/CxbxKrnl/HLEIntercept.cpp
@@ -71,9 +71,10 @@ bool bLLE_JIT = false; // Set this to true for experimental JIT
 bool bXRefFirstPass; // For search speed optimization, set in EmuHLEIntercept, read in EmuLocateFunction
 uint32 UnResolvedXRefs; // Tracks XRef location, used (read/write) in EmuHLEIntercept and EmuLocateFunction
 
-void EmuHLEIntercept(Xbe::LibraryVersion *pLibraryVersion, Xbe::Header *pXbeHeader)
+void EmuHLEIntercept(Xbe::Header *pXbeHeader)
 {
     Xbe::Certificate *pCertificate = (Xbe::Certificate*)pXbeHeader->dwCertificateAddr;
+	Xbe::LibraryVersion *pLibraryVersion = (Xbe::LibraryVersion*)pXbeHeader->dwLibraryVersionsAddr;
 
     char szCacheFileName[MAX_PATH];
 

src/CxbxKrnl/HLEIntercept.h
@@ -38,7 +38,7 @@ extern bool bLLE_APU; // Set this to true for experimental APU (sound) LLE
 extern bool bLLE_GPU; // Set this to true for experimental GPU (graphics) LLE
 extern bool bLLE_JIT; // Set this to true for experimental JIT
 
-void EmuHLEIntercept(Xbe::LibraryVersion *LibraryVersion, Xbe::Header *XbeHeader);
+void EmuHLEIntercept(Xbe::Header *XbeHeader);
 
 #ifdef _DEBUG_TRACE
 void VerifyHLEDataBase();

commit 354b064d60457a780bfd504e5bad38c0093a1181

Fixed a bug introduced in https://github.com/Cxbx-Reloaded/Cxbx-Reloaded/commit/53a731009f7fa4cabd944033fefdb4a1b1a99939

src/CxbxKrnl/CxbxKrnl.cpp
@@ -209,8 +209,12 @@ extern "C" CXBXKRNL_API void CxbxKrnlMain(int argc, char* argv[])
 
 	// Copy over loaded Xbe Header to specified base address
 	memcpy((void*)CxbxKrnl_Xbe->m_Header.dwBaseAddr, &CxbxKrnl_Xbe->m_Header, sizeof(Xbe::Header));	
+	// Copy over the certificate
 	memcpy((void*)(CxbxKrnl_Xbe->m_Header.dwBaseAddr + sizeof(Xbe::Header)), CxbxKrnl_Xbe->m_HeaderEx, CxbxKrnl_Xbe->m_ExSize);
-	
+	// Copy over the library versions
+	memcpy((void*)CxbxKrnl_Xbe->m_Header.dwLibraryVersionsAddr, CxbxKrnl_Xbe->m_LibraryVersion, CxbxKrnl_Xbe->m_Header.dwLibraryVersions * sizeof(DWORD));
+	// TODO : Actually, instead of copying from CxbxKrnl_Xbe, we should load the entire Xbe directly into memory, like Dxbx does.
+
 	// Load Sections
 	for (uint32 i = 0; i < CxbxKrnl_Xbe->m_Header.dwSections; i++) {
 		memcpy((void*)CxbxKrnl_Xbe->m_SectionHeader[i].dwVirtualAddr, CxbxKrnl_Xbe->m_bzSection[i], CxbxKrnl_Xbe->m_SectionHeader[i].dwSizeofRaw);