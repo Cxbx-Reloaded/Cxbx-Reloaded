b61e2f82fdc6a7d881db8164d5bde967b2c7ea25
PatrickvL <pvanlogchem@gmail.com>
2017-03-09 14:29:11 +0100

    Loggin sanitization improvements (less code, better string escaping)

--- src/Common/Logging.cpp
+++ src/Common/Logging.cpp
@@ -39,3 +39,60 @@
 // For thread_local, see : http://en.cppreference.com/w/cpp/language/storage_duration
 // TODO : Use Boost.Format http://www.boost.org/doc/libs/1_53_0/libs/format/index.html
 thread_local std::string _logPrefix;
+
+const bool needs_escape(const wint_t _char)
+{
+	// Escaping is needed for control characters,
+	// for double quote, and for backslash :
+	return iswcntrl(_char) || (_char == '"') || (_char == '\\');
+}
+
+inline void output_char(std::ostream& os, char c)
+{
+	if (needs_escape((int)c))
+	{
+		switch (c)
+		{
+			// Render escaped double quote as \", and escaped backslash as \\ :
+		case '"': os << "\\\""; break;
+		case '\\': os << "\\\\"; break;
+			// See https://en.wikipedia.org/wiki/Escape_sequences_in_C#Table_of_escape_sequences
+		case '\a': os << "\\t"; break;
+		case '\b': os << "\\b"; break;
+		case '\f': os << "\\f"; break;
+		case '\n': os << "\\n"; break;
+		case '\r': os << "\\r"; break;
+		case '\t': os << "\\t"; break;
+		case '\v': os << "\\v"; break;
+			// All other to-escape-characters are rendered as hexadecimal :
+		default: os << "\\x" << std::hex << std::uppercase << (int)c;
+		}
+	}
+	else
+		os << c;
+}
+
+inline void output_wchar(std::ostream& os, wchar_t c)
+{
+	if (needs_escape((wint_t)c))
+	{
+		switch (c)
+		{
+			// Render escaped double quote as \", and escaped backslash as \\ :
+		case '"': os << "\\\""; break;
+		case '\\': os << "\\\\"; break;
+			// See https://en.wikipedia.org/wiki/Escape_sequences_in_C#Table_of_escape_sequences
+		case '\a': os << "\\t"; break;
+		case '\b': os << "\\b"; break;
+		case '\f': os << "\\f"; break;
+		case '\n': os << "\\n"; break;
+		case '\r': os << "\\r"; break;
+		case '\t': os << "\\t"; break;
+		case '\v': os << "\\v"; break;
+			// All other to-escape-characters are rendered as hexadecimal :
+		default: os << "\\x" << std::hex << std::uppercase << (wint_t)c;
+		}
+	}
+	else
+		os << c;
+}
