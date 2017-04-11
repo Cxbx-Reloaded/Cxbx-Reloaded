a86effda6e5100f5a57c309e13c0bbd3ec24c76d
PatrickvL <pvanlogchem@gmail.com>
2017-04-11 11:18:21 +0200

    Documented additional D3D formats for Direct3D 9
    Source :
    https://msdn.microsoft.com/en-us/library/windows/desktop/bb172558(v=vs.85).aspx

--- import/DirectX8/include/d3d8types.h
+++ import/DirectX8/include/d3d8types.h
@@ -1240,6 +1240,14 @@ typedef enum _D3DFORMAT
     D3DFMT_A8                   = 28,
     D3DFMT_A8R3G3B2             = 29,
     D3DFMT_X4R4G4B4             = 30,
+#if 0 // Direct3D 9
+    D3DFMT_A2B10G10R10          = 31,
+    D3DFMT_A8B8G8R8             = 32,
+    D3DFMT_X8B8G8R8             = 33,
+    D3DFMT_G16R16               = 34,
+    D3DFMT_A2R10G10B10          = 35,
+    D3DFMT_A16B16G16R16         = 36,
+#endif
 
     D3DFMT_A8P8                 = 40,
     D3DFMT_P8                   = 41,
@@ -1253,10 +1261,20 @@ typedef enum _D3DFORMAT
     D3DFMT_X8L8V8U8             = 62,
     D3DFMT_Q8W8V8U8             = 63,
     D3DFMT_V16U16               = 64,
+#if 1 // Direct3D 8
     D3DFMT_W11V11U10            = 65,
+#else // Direct3D 9
+    D3DFMT_A2W10V10U10          = 67,
+#endif
 
     D3DFMT_UYVY                 = MAKEFOURCC('U', 'Y', 'V', 'Y'),
+#if 0 // Direct3D 9
+    D3DFMT_R8G8_B8G8            = MAKEFOURCC('R', 'G', 'B', 'G'),
+#endif
     D3DFMT_YUY2                 = MAKEFOURCC('Y', 'U', 'Y', '2'),
+#if 0 // Direct3D 9
+    D3DFMT_G8R8_G8B8            = MAKEFOURCC('G', 'R', 'G', 'B'),
+#endif
     D3DFMT_DXT1                 = MAKEFOURCC('D', 'X', 'T', '1'),
     D3DFMT_DXT2                 = MAKEFOURCC('D', 'X', 'T', '2'),
     D3DFMT_DXT3                 = MAKEFOURCC('D', 'X', 'T', '3'),
@@ -1267,14 +1285,47 @@ typedef enum _D3DFORMAT
     D3DFMT_D32                  = 71,
     D3DFMT_D15S1                = 73,
     D3DFMT_D24S8                = 75,
-    D3DFMT_D16                  = 80,
     D3DFMT_D24X8                = 77,
     D3DFMT_D24X4S4              = 79,
+    D3DFMT_D16                  = 80,
 
+#if 0 // Direct3D 9
+    D3DFMT_D32F_LOCKABLE        = 82,
+    D3DFMT_D24FS8               = 83,
+
+#if !defined(D3D_DISABLE_9EX)
+    D3DFMT_D32_LOCKABLE         = 84,
+    D3DFMT_S8_LOCKABLE          = 85,
+#endif // !D3D_DISABLE_9EX
+
+    D3DFMT_L16                  = 81,
+#endif
 
     D3DFMT_VERTEXDATA           =100,
     D3DFMT_INDEX16              =101,
     D3DFMT_INDEX32              =102,
+
+#if 0 // Direct3D 9
+    D3DFMT_Q16W16V16U16         =110,
+
+    D3DFMT_MULTI2_ARGB8         = MAKEFOURCC('M','E','T','1'),
+
+    D3DFMT_R16F                 = 111,
+    D3DFMT_G16R16F              = 112,
+    D3DFMT_A16B16G16R16F        = 113,
+
+    D3DFMT_R32F                 = 114,
+    D3DFMT_G32R32F              = 115,
+    D3DFMT_A32B32G32R32F        = 116,
+
+    D3DFMT_CxV8U8               = 117,
+
+#if !defined(D3D_DISABLE_9EX)
+    D3DFMT_A1                   = 118,
+    D3DFMT_A2B10G10R10_XR_BIAS  = 119,
+    D3DFMT_BINARYBUFFER         = 199,
+#endif // !D3D_DISABLE_9EX
+#endif
 
     D3DFMT_FORCE_DWORD          =0x7fffffff
 } D3DFORMAT;
