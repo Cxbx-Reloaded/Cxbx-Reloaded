b61f9327d75d9a223a0677ca028d03524dd2ea5b
jarupxx <jarup@gmx.com>
2017-04-13 19:04:28 +0900

    Renamed XLaunchNewImage to XLaunchNewImageA

--- src/CxbxKrnl/EmuXapi.h
+++ src/CxbxKrnl/EmuXapi.h
@@ -596,9 +596,9 @@ BOOL WINAPI EMUPATCH(GetOverlappedResult)
 );
 
 // ******************************************************************
-// * patch: XLaunchNewImage
+// * patch: XLaunchNewImageA
 // ******************************************************************
-DWORD WINAPI EMUPATCH(XLaunchNewImage)
+DWORD WINAPI EMUPATCH(XLaunchNewImageA)
 (
 	LPCSTR			lpTitlePath,
 	PLAUNCH_DATA	pLaunchData
