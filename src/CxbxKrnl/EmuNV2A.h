0df20a488811baca2d287fcfe2f1d32e1191f0e0
Melissa Goad <alegend45@gmail.com>
2017-07-30 05:44:40 -0500

    Ooops

--- src/CxbxKrnl/EmuNV2A.h
+++ src/CxbxKrnl/EmuNV2A.h
@@ -42,9 +42,9 @@
 
 typedef struct {
 	DWORD Ignored[0x10];
-	PPUSH Put; // On Xbox1, this field is only written to by the CPU (the GPU uses this as a trigger to start executing from the given address)
-	PPUSH Get; // On Xbox1, this field is only read from by the CPU (the GPU reflects in here where it is/stopped executing)
-	PPUSH Reference; // TODO : xbaddr / void* / DWORD ? 
+	DWORD Put; // On Xbox1, this field is only written to by the CPU (the GPU uses this as a trigger to start executing from the given address)
+	DWORD Get; // On Xbox1, this field is only read from by the CPU (the GPU reflects in here where it is/stopped executing)
+	DWORD Reference; // TODO : xbaddr / void* / DWORD ? 
 	DWORD Ignored2[0x7ED];
 } Nv2AControlDma;
 
