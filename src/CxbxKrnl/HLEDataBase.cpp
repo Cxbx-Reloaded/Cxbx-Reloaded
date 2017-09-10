b4bad9e4d8e926804abb031e37dbea789fa8ff3f
jarupxx <jarup@gmx.com>
2017-09-11 06:39:12 +0900

    Enable XACTENG_OOVPAV2

--- src/CxbxKrnl/HLEDataBase.cpp
+++ src/CxbxKrnl/HLEDataBase.cpp
@@ -218,7 +218,7 @@ const HLEDataV2 HLEDataBaseV2[] = {
     //{ Lib_DSOUNDH,{ Sec_DSOUND }, DSound_OOVPAV2, DSound_OOVPA_SIZEV2 },
 
     //
-    //{ Lib_XACTENG, { Sec_XACTENG }, XACTENG_OOVPAV2, XACTENG_OOVPA_SIZEV2 },
+    { Lib_XACTENG, { Sec_XACTENG }, XACTENG_OOVPAV2, XACTENG_OOVPA_SIZEV2 },
 
     // test case: Power Drome (Unluckily, it use LTCG version of the library.)
     //{ Lib_XACTENLT,{ Sec_XACTENG }, XACTENG_OOVPAV2, XACTENG_OOVPA_SIZEV2 },
