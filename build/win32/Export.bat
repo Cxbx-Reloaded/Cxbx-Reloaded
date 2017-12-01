2f1a0405573ca7b69760a54c323b9cc3996cf392
jarupxx <jarup@gmx.com>
2017-12-01 21:35:16 +0900

    Check artifact before exporting

--- build/win32/Export.bat
+++ build/win32/Export.bat
@@ -1,5 +1,10 @@
+if exist Release\Cxbx.exe (
 cd Release
 ..\..\..\import\7za\7za.exe a ..\..\..\export\Release.zip Cxbx.exe glew32.dll subhook.dll ..\..\..\COPYING ..\..\..\README.md
-cd ..\Debug
+cd ..\
+)
+if exist Debug\Cxbx.exe (
+cd Debug
 ..\..\..\import\7za\7za.exe a ..\..\..\export\Debug.zip Cxbx.exe glew32.dll subhook.dll ..\..\..\COPYING ..\..\..\README.md
 cd ..\
+)
