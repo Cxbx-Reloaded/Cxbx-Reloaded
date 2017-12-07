56ac7f86a4eebbfcfe9c8928fe764b01af5ca92a
x1nixmzeng <x1nixmzeng@live.co.uk>
2017-12-07 09:26:56 +0000

    Rewrite of packager script so it can be run per configuration

--- AppveyorPackager.ps1
+++
@@ -1,6 +0,0 @@
-if exist build\win32\$(configuration)\Cxbx.exe (
-7z a export\$(configuration).zip COPYING README.md
-cd build\win32\$(configuration)
-7z u ..\..\..\export\$(configuration).zip Cxbx.exe glew32.dll subhook.dll
-cd ..\..\..\
-)
