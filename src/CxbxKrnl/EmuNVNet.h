ec01f039a5646270573a72a4b8678fedeffda8b3
PatrickvL <pvanlogchem@gmail.com>
2017-12-15 18:37:09 +0100

    NVNet : Corrected bar order and a typo

--- src/CxbxKrnl/EmuNVNet.h
+++ src/CxbxKrnl/EmuNVNet.h
@@ -44,7 +44,7 @@ public:
 	void Init();
 	void Reset();
 	uint32_t IORead(int barIndex, uint32_t port, unsigned size);
-	void IOWrite(int BarIndex, uint32_t port, uint32_t value, unsigned size);
+	void IOWrite(int barIndex, uint32_t port, uint32_t value, unsigned size);
 	uint32_t MMIORead(int barIndex, uint32_t addr, unsigned size);
 	void MMIOWrite(int barIndex, uint32_t addr, uint32_t value, unsigned size);
 };
