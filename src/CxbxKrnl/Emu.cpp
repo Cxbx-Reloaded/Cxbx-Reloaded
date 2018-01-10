b6c243d7978cde7f0f442b4df611fff2c9656517
Luke Usher <luke.usher@outlook.com>
2018-01-10 11:34:05 +0000

    Prep for Pixel Shaders Disable Hack
    - Implements error checking/default value enforcement on registry reads
    - Adds a new option (currently does nothing) for enabling the hack.

--- src/CxbxKrnl/Emu.cpp
+++ src/CxbxKrnl/Emu.cpp
@@ -70,6 +70,7 @@ volatile thread_local  bool    g_bEmuException = false;
 volatile bool    g_bEmuSuspended = false;
 volatile bool    g_bPrintfOn = true;
 bool g_XInputEnabled = false;
+bool g_DisablePixelShaders = false;
 
 // Delta added to host SystemTime, used in xboxkrnl::KeQuerySystemTime and xboxkrnl::NtSetSystemTime
 LARGE_INTEGER	HostSystemTimeDelta = {};
