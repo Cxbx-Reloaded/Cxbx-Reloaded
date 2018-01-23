3314fc18fb14cb9e0a1b4dc283acc3c3ab20c44b
PatrickvL <pvanlogchem@gmail.com>
2018-01-23 16:26:01 +0100

    NV2A : Added defines and code from my WIP_LessVertexPatching branch that wasn't yet in master. Also some defines from the OpenXbox repository.

--- src/devices/video/EmuNV2A.h
+++ src/devices/video/EmuNV2A.h
@@ -45,7 +45,7 @@
 #define NV_PBUS_ADDR     0x00001000
 #define NV_PBUS_SIZE                0x001000
 #define NV_PFIFO_ADDR    0x00002000
-#define NV_PFIFO_SIZE               0x002000
+#define _NV_PFIFO_SIZE               0x002000 // Underscore prefix to prevent clash with NV_PFIFO_SIZE
 #define NV_PRMA_ADDR     0x00007000
 #define NV_PRMA_SIZE                0x001000
 #define NV_PVIDEO_ADDR   0x00008000
@@ -61,7 +61,7 @@
 #define NV_PRMFB_ADDR    0x000A0000
 #define NV_PRMFB_SIZE               0x020000
 #define NV_PRMVIO_ADDR   0x000C0000
-#define NV_PRMVIO_SIZE              0x001000
+#define NV_PRMVIO_SIZE              0x008000 // Was 0x001000
 #define NV_PFB_ADDR      0x00100000
 #define NV_PFB_SIZE                 0x001000
 #define NV_PSTRAPS_ADDR  0x00101000
@@ -79,18 +79,65 @@
 #define NV_PRAMIN_ADDR   0x00700000
 #define NV_PRAMIN_SIZE              0x100000
 #define NV_USER_ADDR     0x00800000
-#define NV_USER_SIZE                0x800000
+#define NV_USER_SIZE                0x400000
+#define NV_UREMAP_ADDR   0x00C00000 // Looks like a mapping of NV_USER_ADDR
+#define NV_UREMAP_SIZE              0x400000
+
+typedef volatile DWORD *PPUSH;
 
 typedef struct {
 	DWORD Ignored[0x10];
-	DWORD* Put; // On Xbox1, this field is only written to by the CPU (the GPU uses this as a trigger to start executing from the given address)
-	DWORD* Get; // On Xbox1, this field is only read from by the CPU (the GPU reflects in here where it is/stopped executing)
-	DWORD Reference; // TODO : xbaddr / void* / DWORD ? 
+	PPUSH Put; // On Xbox1, this field is only written to by the CPU (the GPU uses this as a trigger to start executing from the given address)
+	PPUSH Get; // On Xbox1, this field is only read from by the CPU (the GPU reflects in here where it is/stopped executing)
+	PPUSH Reference; // TODO : xbaddr / void* / DWORD ? 
 	DWORD Ignored2[0x7ED];
 } Nv2AControlDma;
 
 uint32_t EmuNV2A_Read(xbaddr addr, int size);
 void EmuNV2A_Write(xbaddr addr, uint32_t value, int size);
+
+#define PUSH_TYPE_MASK         0x00000002 // 2 bits
+#define PUSH_TYPE_SHIFT        0
+#define PUSH_TYPE_METHOD        0 // method
+#define PUSH_TYPE_JMP_FAR       1 // jump far
+#define PUSH_TYPE_CALL_FAR      2 // call far
+#define PUSH_TYPE_METHOD_UNUSED 3 // method (unused)
+#define PUSH_METHOD_MASK       0x00001FFC // 12 bits
+#define PUSH_METHOD_SHIFT      0 // Dxbx note : Not 2, because methods are actually DWORD offsets (and thus defined with increments of 4)
+#define PUSH_SUBCH_MASK        0x0000E000 // 3 bits
+#define PUSH_SUBCH_SHIFT       13
+#define PUSH_COUNT_MASK        0x1FFC0000 // 11 bits
+#define PUSH_COUNT_SHIFT       18
+#define PUSH_INSTR_MASK        0xE0000000 // 3 bits
+#define PUSH_INSTR_SHIFT       29
+#define PUSH_INSTR_IMM_INCR     0 // immediate, increment
+#define PUSH_INSTR_JMP_NEAR     1 // near jump
+#define PUSH_INSTR_IMM_NOINC    2 // immediate, no-increment
+#define PUSH_ADDR_FAR_MASK     0xFFFFFFFC // 30 bits
+#define PUSH_ADDR_FAR_SHIFT    0
+#define PUSH_ADDR_NEAR_MASK    0x1FFFFFFC // 27 bits
+#define PUSH_ADDR_NEAR_SHIFT   0 // Cxbx note : Not 2, because methods are actually DWORD offsets (and thus defined with increments of 4)
+
+#define PUSH_TYPE(dwPushCommand) ((dwPushCommand & PUSH_TYPE_MASK) >> PUSH_TYPE_SHIFT)
+#define PUSH_METHOD(dwPushCommand) ((dwPushCommand & PUSH_METHOD_MASK) >> PUSH_METHOD_SHIFT)
+#define PUSH_SUBCH(dwPushCommand) ((dwPushCommand & PUSH_SUBCH_MASK) >> PUSH_SUBCH_SHIFT)
+#define PUSH_COUNT(dwPushCommand) ((dwPushCommand & PUSH_COUNT_MASK) >> PUSH_COUNT_SHIFT)
+#define PUSH_INSTR(dwPushCommand) ((dwPushCommand & PUSH_INSTR_MASK) >> PUSH_INSTR_SHIFT)
+#define PUSH_ADDR_FAR(dwPushCommand) ((dwPushCommand & PUSH_ADDR_FAR_MASK) >> PUSH_ADDR_FAR_SHIFT)
+#define PUSH_ADDR_NEAR(dwPushCommand) ((dwPushCommand & PUSH_ADDR_NEAR_MASK) >> PUSH_ADDR_NEAR_SHIFT)
+
+#define PUSH_METHOD_MAX ((PUSH_METHOD_MASK | 3) >> PUSH_METHOD_SHIFT) // = 8191
+#define PUSH_SUBCH_MAX (PUSH_SUBCH_MASK >> PUSH_SUBCH_SHIFT) // = 7
+#define PUSH_COUNT_MAX (PUSH_COUNT_MASK >> PUSH_COUNT_SHIFT) // = 2047
+
+// Decode push buffer conmmand (inverse of D3DPUSH_ENCODE)
+inline void D3DPUSH_DECODE(const DWORD dwPushCommand, DWORD &dwMethod, DWORD &dwSubCh, DWORD &dwCount)
+{
+	dwMethod = PUSH_METHOD(dwPushCommand);
+	dwSubCh = PUSH_SUBCH(dwPushCommand);
+	dwCount = PUSH_COUNT(dwPushCommand);
+}
+
 void EmuNV2A_Init();
 
 void InitOpenGLContext();
