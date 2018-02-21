bc176edc4ca12d26373740a6361d8987f6908ba7
x1nixmzeng <x1nixmzeng@live.co.uk>
2018-02-21 08:22:08 +0000

    Add AppVeyor logging to MSBuild

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -4,24 +4,31 @@
 
 @set SLN="%cd%\build\win32\Cxbx.sln"
 @set VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+@set VS_LOGGER="C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
 :: Setup build environment
 @if not exist %VS_ENV% goto missing_env
+@if not exist %VS_LOGGER% goto missing_log
 
 @call %VS_ENV%
 
 :: Build managed projects (debugger)
-@call MSBuild.exe %SLN% /p:Platform="Any CPU" /p:Configuration=%1% /m
+@call MSBuild.exe %SLN% /p:Platform="Any CPU" /p:Configuration=%1% /m /logger:%VS_LOGGER%
 @if %errorlevel% neq 0 goto failure
 
 :: Build native projects (emulator, debugger dlls)
-@call MSBuild.exe %SLN% /p:Platform="Win32" /p:Configuration=%1% /m
+@call MSBuild.exe %SLN% /p:Platform="Win32" /p:Configuration=%1% /m /logger:%VS_LOGGER%
 @if %errorlevel% neq 0 goto failure
 
 @goto success
 
 :missing_env
 @echo Unable to locate MSBuild environment command
+@set ERRORLEVEL=1
+@goto eof
+
+:missing_log
+@echo Unable to locate AppVeyor build logger
 @set ERRORLEVEL=1
 @goto eof
 
