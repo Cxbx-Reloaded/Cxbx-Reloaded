bda64774f34a40c600010db2d41a942fb11edc67
Luke Usher <luke.usher@outlook.com>
2018-03-16 13:27:15 +0000

    Fix Debug Mode compilation

--- src/HighPerformanceGraphicsEnabler.c
+++ src/HighPerformanceGraphicsEnabler.c
@@ -1,4 +1,5 @@
 #include <Windows.h>
 
 // Default to High Performance Mode on machines with dual graphics
-__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;	// AMD__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;		// NVIDIA
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;	// AMD
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;		// NVIDIA
