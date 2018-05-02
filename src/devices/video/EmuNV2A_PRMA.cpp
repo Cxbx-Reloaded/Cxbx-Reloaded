989429bcc64a27201bfdf9d2a5dcd4828fcf9d53
ergo720 <csteven129@gmail.com>
2018-05-02 12:58:21 +0200

    Added missing copyright text

--- src/devices/video/EmuNV2A_PRMA.cpp
+++ src/devices/video/EmuNV2A_PRMA.cpp
@@ -1,7 +1,48 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// ******************************************************************
+// *
+// *    .,-:::::    .,::      .::::::::.    .,::      .:
+// *  ,;;;'````'    `;;;,  .,;;  ;;;'';;'   `;;;,  .,;;
+// *  [[[             '[[,,[['   [[[__[[\.    '[[,,[['
+// *  $$$              Y$$$P     $$""""Y$$     Y$$$P
+// *  `88bo,__,o,    oP"``"Yo,  _88o,,od8P   oP"``"Yo,
+// *    "YUMMMMMP",m"       "Mm,""YUMMMP" ,m"       "Mm,
+// *
+// *   src->devices->video->EmuNV2A_PRMA.cpp
+// *
+// *  This file is part of the Cxbx project.
+// *
+// *  Cxbx and Cxbe are free software; you can redistribute them
+// *  and/or modify them under the terms of the GNU General Public
+// *  License as published by the Free Software Foundation; either
+// *  version 2 of the license, or (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful,
+// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *  GNU General Public License for more details.
+// *
+// *  You should have recieved a copy of the GNU General Public License
+// *  along with this program; see the file COPYING.
+// *  If not, write to the Free Software Foundation, Inc.,
+// *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
+// *
+// *  nv2a.cpp is heavily based on code from XQEMU
+// *  Copyright(c) 2012 espes
+// *  Copyright(c) 2015 Jannik Vogel
+// *  https://github.com/espes/xqemu/blob/xbox/hw/xbox/nv2a.c
+// *  (c) 2017-2018 Luke Usher <luke.usher@outlook.com>
+// *  (c) 2018 Patrick van Logchem <pvanlogchem@gmail.com>
+// *
+// *  All rights reserved
+// *
+// ******************************************************************
 
 // TODO: Remove disabled warning once case are add to PRMA switch.
 #pragma warning(push)
 #pragma warning(disable: 4065)
+
 DEVICE_READ32(PRMA)
 {
 	DEVICE_READ32_SWITCH() {
