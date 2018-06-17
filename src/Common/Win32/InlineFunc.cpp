3b9939f55edb6a74aa8bdd0ef1d70f209a634189
RadWolfie <RadWolfie@users.noreply.github.com>
2018-06-16 20:26:24 -0500

    Use shorter link to source.

--- src/Common/Win32/InlineFunc.cpp
+++ src/Common/Win32/InlineFunc.cpp
@@ -49,7 +49,7 @@ bool CxbxExec(std::string &execCommand, HANDLE* hProcess, bool requestHandleProc
 	szArgsBufferOutput[szSize] = '\0';
 
     /* NOTE: CreateProcess's 2nd parameter (lpCommandLine) is char*, not const char*. Plus it has ability to change the input buffer data.
-       Source: https://msdn.microsoft.com/en-us/library/windows/desktop/ms682425(v=vs.85).aspx
+       Source: https://msdn.microsoft.com/en-us/library/ms682425.aspx
 
        Using ShellExecute has proper implement. Unfortunately, we need created process handle for Debugger monitor.
        Plus ShellExecute is high level whilst CreateProcess is low level. We want to use official low level functions as possible to reduce
