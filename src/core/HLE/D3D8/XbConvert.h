d47b4a69d4ea3b7261722fa3f2434782b766b566
Luke Usher <luke.usher@outlook.com>
2018-10-13 22:38:18 +0100

    Cleanup/Simplify deferred render state handling

--- src/core/HLE/D3D8/XbConvert.h
+++ src/core/HLE/D3D8/XbConvert.h
@@ -1787,9 +1787,9 @@ typedef enum _TXBType {
 typedef struct _RenderStateInfo {
 	char *S;   // String representation.
 	WORD V;    // The XDK version since which a render state was introduced (using the 5911 declarations as a base).
-	TXBType T; // The Xbox data type. Defaults to xt_Unknown.
-	NV2AMETHOD M;   // The related push buffer method. Not always a 1-to-1 mapping. Needs push-buffer interpretation & conversion code.
-	D3DRENDERSTATETYPE PC; // Map XBox to PC render state. Defaults to D3DRS_UNSUPPORTED.
+	TXBType T = xt_Unknown; // The Xbox data type. Defaults to xt_Unknown.
+	NV2AMETHOD M; // The related push buffer method. Not always a 1-to-1 mapping. Needs push-buffer interpretation & conversion code.
+	D3DRENDERSTATETYPE PC = (D3DRENDERSTATETYPE)0; // Map XBox to PC render state
 	char *N;   // XDK notes. Defaults to ''.
 }
 RenderStateInfo;
