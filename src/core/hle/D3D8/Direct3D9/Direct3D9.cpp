6f74eacf26a17cc7cc7772a4137c1f2eb8730bc6
patrickvl <pvanlogchem@gmail.com>
2019-02-10 16:57:59 +0100

    Loader : Deduplicate code via new CreateSettings()

--- src/core/hle/D3D8/Direct3D9/Direct3D9.cpp
+++ src/core/hle/D3D8/Direct3D9/Direct3D9.cpp
@@ -26,7 +26,7 @@
 // ******************************************************************
 #define LOG_PREFIX CXBXR_MODULE::D3D8
 
-#ifdef CXBXEMULATOR_EXPORTS // DbgConsole only in Cxbx, not in CxbxEmulator
+#ifdef CXBXR_EMU_EXPORTS // DbgConsole only in Cxbx/cxbxr, not in cxbxr-emu
 	#undef INCLUDE_DBG_CONSOLE
 #else
 	#define INCLUDE_DBG_CONSOLE
