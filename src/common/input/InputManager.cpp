254358e9943ba1fb41e8ab91d27ae0c1adae6f40
ergo720 <45463469+ergo720@users.noreply.github.com>
2019-02-24 22:07:11 +0100

    Added missing copyright texts

--- src/common/input/InputManager.cpp
+++ src/common/input/InputManager.cpp
@@ -19,7 +19,7 @@
 // *  If not, write to the Free Software Foundation, Inc.,
 // *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
 // *
-// *  (c) 2018 ergo720
+// *  (c) 2019 ergo720
 // *
 // *  All rights reserved
 // *
--- src/common/input/SdlJoystick.h
+++ src/common/input/SdlJoystick.h
@@ -2,15 +2,6 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 // ******************************************************************
 // *
-// *    .,-:::::    .,::      .::::::::.    .,::      .:
-// *  ,;;;'````'    `;;;,  .,;;  ;;;'';;'   `;;;,  .,;;
-// *  [[[             '[[,,[['   [[[__[[\.    '[[,,[['
-// *  $$$              Y$$$P     $$""""Y$$     Y$$$P
-// *  `88bo,__,o,    oP"``"Yo,  _88o,,od8P   oP"``"Yo,
-// *    "YUMMMMMP",m"       "Mm,""YUMMMP" ,m"       "Mm,
-// *
-// *   src->Common->Input->SdlJoystick.h
-// *
 // *  This file is part of the Cxbx project.
 // *
 // *  Cxbx and Cxbe are free software; you can redistribute them
@@ -28,7 +19,7 @@
 // *  If not, write to the Free Software Foundation, Inc.,
 // *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
 // *
-// *  (c) 2018 ergo720
+// *  (c) 2019 ergo720
 // *
 // *  All rights reserved
 // *
