5e1851fcad68917b4847c54ee1f96eb963c5b723
Anthony Miles <a.miles.nz@gmail.com>
2019-07-20 18:49:24 +1200

    Preserve caller esp value

--- src/core/kernel/support/EmuFS.cpp
+++ src/core/kernel/support/EmuFS.cpp
@@ -421,13 +421,16 @@ __declspec(naked) void EmuFS_MovFs00Esp()
 	// Note : eax must be preserved here, hence the push/pop
 	__asm
 	{
+		pushfd
 		call LockFS
 		call EmuFS_RefreshKPCR
 		push eax
 		mov eax, fs : [TIB_ArbitraryDataSlot]
 		mov [eax], esp
+		add [eax], 12 // account for esp changes from pushed registers and return address
 		pop eax
 		call UnlockFS
+		popfd
 		ret
 	}
 }
