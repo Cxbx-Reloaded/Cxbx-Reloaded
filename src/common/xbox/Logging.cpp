e6f875b7a7242df9f5df23a91597d309358fa46d
RadWolfie <RadWolfie@users.noreply.github.com>
2020-08-26 01:05:11 -0500

    dsound: updated to use without depend on windows' dsound header file for xbox usage

---
+++ src/common/xbox/Logging.cpp
@@ -0,0 +1,42 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// ******************************************************************
+// *
+// *  This file is part of the Cxbx project.
+// *
+// *  Cxbx and Cxbe are free software; you can redistribute them
+// *  and/or modify them under the terms of the GNU General Public
+// *  License as published by the Free Software Foundation; either
+// *  version 2 of the license, or (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful,
+// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *  GNU General Public License for more details.
+// *
+// *  You should have recieved a copy of the GNU General Public License
+// *  along with this program; see the file COPYING.
+// *  If not, write to the Free Software Foundation, Inc.,
+// *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
+// *
+// *  (c) 2020 RadWolfie
+// *
+// *  All rights reserved
+// *
+// ******************************************************************
+
+#include "common/Logging.h"
+#include "Logging.hpp"
+
+namespace xbox {
+
+LOGRENDER(D3DVECTOR)
+{
+	return os
+		LOGRENDER_MEMBER(x)
+		LOGRENDER_MEMBER(y)
+		LOGRENDER_MEMBER(z)
+		;
+}
+
+}
