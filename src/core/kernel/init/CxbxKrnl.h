d7f0957cbab5f7f90bc38141d8f6ec692c40dde1
Anthony <a.miles.nz@gmail.com>
2021-01-19 01:47:02 +1300

    Note why LOG_TEST_CASE is where it is

--- src/core/kernel/init/CxbxKrnl.h
+++ src/core/kernel/init/CxbxKrnl.h
@@ -237,6 +237,7 @@ extern std::string CxbxGetLastErrorString(char * lpszFunction);
 
 // The reason of having EmuLogOutputEx in LOG_TEST_CASE is to allow dump to log directly for any test cases triggered.
 // Which will make developers easier to note which applications has triggered quicker, easier, and doesn't require any individual log enabled to capture them.
+// NOTE: This #define is here rather than Logging.h, because it has a dependency on CxbxKrnl_Xbe
 #define LOG_TEST_CASE(message) do { \
 	static bool bTestCaseLogged = false; \
 	if (bTestCaseLogged) break; \
