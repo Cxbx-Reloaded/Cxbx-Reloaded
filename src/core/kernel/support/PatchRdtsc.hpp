e50ba6b6277936ac6c669ad94d6b3902afc87368
RadWolfie <RadWolfie@users.noreply.github.com>
2021-06-30 15:39:23 -0500

    kernel: move rdtsc patches relative into its own source file.

---
+++ src/core/kernel/support/PatchRdtsc.hpp
@@ -0,0 +1,34 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// ******************************************************************
+// *
+// *  This file is part of the Cxbx-Reloaded project.
+// *
+// *  Cxbx and Cxbe are free software; you can redistribute them
+// *  and/or modify them under the terms of the GNU General Public
+// *  License as published by the Free Software Foundation; either
+// *  version 2 of the license, or (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful,
+// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *  GNU General Public License for more details.
+// *
+// *  You should have recieved a copy of the GNU General Public License
+// *  along with this program; see the file COPYING.
+// *  If not, write to the Free Software Foundation, Inc.,
+// *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
+// *
+// *  All rights reserved
+// *
+// ******************************************************************
+#pragma once
+
+#include "core\kernel\exports\xboxkrnl.h"
+
+// NOTE: This file is intended to be used only with Cxbxkrnl.cpp file.
+// NOTE2: If need to include header, please do in Cxbxkrnl.cpp file.
+
+bool IsRdtscInstruction(xbox::addr_xt addr);
+
+void PatchRdtscInstructions();
