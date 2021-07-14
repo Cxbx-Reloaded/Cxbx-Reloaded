cb3330a9e42aa2049fcef6a5c147443e536221d5
ergo720 <45463469+ergo720@users.noreply.github.com>
2021-07-14 15:38:45 +0200

    Removed stdcall from EmuLog

--- src/common/Logging.cpp
+++ src/common/Logging.cpp
@@ -159,7 +159,7 @@ inline void EmuLogOutputEx(const CXBXR_MODULE cxbxr_module, const LOG_LEVEL leve
 }
 
 // print out a custom message to the console or kernel debug log file
-void NTAPI EmuLogEx(CXBXR_MODULE cxbxr_module, LOG_LEVEL level, const char *szWarningMessage, ...)
+void EmuLogEx(CXBXR_MODULE cxbxr_module, LOG_LEVEL level, const char *szWarningMessage, ...)
 {
 	if (szWarningMessage == NULL) {
 		return;
@@ -180,7 +180,7 @@ void NTAPI EmuLogEx(CXBXR_MODULE cxbxr_module, LOG_LEVEL level, const char *szWa
 	}
 }
 
-void NTAPI EmuLogInit(LOG_LEVEL level, const char *szWarningMessage, ...)
+void EmuLogInit(LOG_LEVEL level, const char *szWarningMessage, ...)
 {
 	if (szWarningMessage == NULL) {
 		return;
