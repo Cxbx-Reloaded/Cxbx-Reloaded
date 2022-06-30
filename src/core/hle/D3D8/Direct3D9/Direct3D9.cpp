8546d7c10d0c7b92f6405d40f7caabc3b58aea5d
PatrickvL <pvanlogchem@gmail.com>
2022-06-30 11:33:29 +0200

    EMUPATCH(D3DDevice_RunVertexStateShader): added LOG_TEST_CASE precondition checks, replacing an assert simplified implementation (no need for an intermediate variable) commented some notes and future suggestions

--- src/core/hle/D3D8/Direct3D9/Direct3D9.cpp
+++ src/core/hle/D3D8/Direct3D9/Direct3D9.cpp
@@ -8681,35 +8681,41 @@ xbox::void_xt WINAPI xbox::EMUPATCH(D3DDevice_RunVertexStateShader)
 
 	// If pData is assigned, pData[0..3] is pushed towards nv2a transform data registers
 	// then sends the nv2a a command to launch the vertex shader function located at Address
+	if (Address >= NV2A_MAX_TRANSFORM_PROGRAM_LENGTH) {
+		LOG_TEST_CASE("Address out of bounds");
+		return;
+	}
+
 	NV2AState* dev = g_NV2A->GetDeviceState();
 	PGRAPHState* pg = &(dev->pgraph);
 
-	float vertex_state_shader_v0[4];
-	if (pData != nullptr)
-		//if pData != nullptr, then it contents v0.xyzw, we shall copy the binary content directly.
-		memcpy(vertex_state_shader_v0, pData, sizeof(vertex_state_shader_v0));
-	else
-		//for pData == nullptr, this data is not supposed to be used. but we assign v0.xyzw 0.0f in each component just in case.
-		std::memset(vertex_state_shader_v0, 0, sizeof(vertex_state_shader_v0));
-
-	int shader_slot = Address;
-	Nv2aVshProgram program;
-
+	Nv2aVshProgram program = {}; // Note: This nulls program.steps
+	// TODO : Retain program globally and perform nv2a_vsh_parse_program only when
+	//        the address-range we're about to emulate was modified since last parse.
+	// TODO : As a suggestion for this, parse all NV2A_MAX_TRANSFORM_PROGRAM_LENGTH slots,
+	//        and here just point program.steps to global vsh_program_steps[Address].
 	Nv2aVshParseResult result = nv2a_vsh_parse_program(
-		&program,
-		//pg->program_data[shader_slot],
-		GetCxbxVertexShaderSlotPtr(shader_slot),
-		NV2A_MAX_TRANSFORM_PROGRAM_LENGTH - shader_slot);
-	assert(result == NV2AVPR_SUCCESS);
+		&program, // Note : program.steps will be malloc'ed
+		GetCxbxVertexShaderSlotPtr(Address), // TODO : At some point, use pg->program_data[Address] here instead
+		NV2A_MAX_TRANSFORM_PROGRAM_LENGTH - Address);
+	if (result != NV2AVPR_SUCCESS) {
+		LOG_TEST_CASE("nv2a_vsh_parse_program failed");
+		// TODO : Dump Nv2aVshParseResult as string and program for debugging purposes
+		return;
+	}
 
 	Nv2aVshCPUXVSSExecutionState state_linkage;
 	Nv2aVshExecutionState state = nv2a_vsh_emu_initialize_xss_execution_state(
-		&state_linkage, (float*)pg->vsh_constants);
-	memcpy(state_linkage.input_regs, vertex_state_shader_v0, sizeof(vertex_state_shader_v0));
+		&state_linkage, (float*)pg->vsh_constants); // Note : This wil memset(state_linkage, 0)
+	if (pData != nullptr)
+		//if pData != nullptr, then it contains v0.xyzw, we shall copy the binary content directly.
+		memcpy(state_linkage.input_regs, pData, sizeof(state_linkage.input_regs));
 
 	nv2a_vsh_emu_execute_track_context_writes(&state, &program, pg->vsh_constants_dirty);
+	// Note: Above emulation's primary purpose is to update pg->vsh_constants and pg->vsh_constants_dirty
+	// therefor, nothing else needs to be done here, other than to cleanup
 
-	nv2a_vsh_program_destroy(&program);	
+	nv2a_vsh_program_destroy(&program); // Note: program.steps will be free'ed
 }
 
 // ******************************************************************
