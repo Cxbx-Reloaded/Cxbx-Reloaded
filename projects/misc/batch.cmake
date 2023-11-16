ae140bb6bf48c4e3f34e5d44efefd6c81b29fff6
Anthony <a.miles.nz@gmail.com>
2023-11-16 23:00:18 +1300

    Copy and install hlsl files in the cxbx project

--- projects/misc/batch.cmake
+++ projects/misc/batch.cmake
@@ -27,14 +27,3 @@ message("Runtime Build Directory: ${TargetRunTimeDir}")
 # Copy glew32.dll to build type's folder.
 set(CXBXR_GLEW_DLL "${CMAKE_SOURCE_DIR}/import/glew-2.0.0/bin/Release/Win32/glew32.dll")
 file(COPY ${CXBXR_GLEW_DLL} DESTINATION  ${TargetRunTimeDir})
-
-# Copy HLSL files to the output directory, which are loaded at runtime
-FILE(GLOB CXBXR_HLSL_FILES "${CMAKE_SOURCE_DIR}/src/core/hle/D3D8/Direct3D9/*.hlsl*")
-set(HlslOutputDir ${TargetRunTimeDir}/hlsl)
-file(MAKE_DIRECTORY ${HlslOutputDir})
-file(COPY ${CXBXR_HLSL_FILES} DESTINATION ${HlslOutputDir})
-# These files can be edited.
-# Create backup copies for convenience of restoring original shader behaviour.
-set(HlslBackupOutputDir ${HlslOutputDir}/backup)
-file(MAKE_DIRECTORY ${HlslBackupOutputDir})
-file(COPY ${CXBXR_HLSL_FILES} DESTINATION ${HlslBackupOutputDir})
