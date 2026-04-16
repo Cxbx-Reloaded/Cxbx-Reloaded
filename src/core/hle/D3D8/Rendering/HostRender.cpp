86eab8a4a31b77e1f75bd6d91243d49a1e92c0a9
PatrickvL <PatrickvL@users.noreply.github.com>
2026-04-16 11:47:23 +0200

    Extract CxbxImGui_RenderD3D() from HostRender.cpp to HostImGui.cpp
    ImGui rendering is conceptually separate from render target management
    and fixed-function lighting state that make up the rest of HostRender.
    Move it to its own file for clarity.

--- src/core/hle/D3D8/Rendering/HostRender.cpp
+++ src/core/hle/D3D8/Rendering/HostRender.cpp
@@ -1,41 +1,6 @@
 #include "EmuD3D8_common.h"
 
 
-void CxbxImGui_RenderD3D(ImGuiUI* m_imgui, IDirect3DSurface* renderTarget)
-{
-#ifdef CXBX_USE_D3D11
-	ImGui_ImplDX11_NewFrame();
-#else
-	ImGui_ImplDX9_NewFrame();
-#endif
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-
-	m_imgui->DrawMenu();
-	m_imgui->DrawWidgets();
-
-	ImGui::EndFrame();
-
-	ImGui::Render();
-	ImDrawData* drawData = ImGui::GetDrawData();
-	if (drawData->TotalVtxCount > 0) {
-#ifdef CXBX_USE_D3D11
-		(void)CxbxSetRenderTarget(renderTarget);
-		ImGui_ImplDX11_RenderDrawData(drawData);
-		(void)CxbxSetRenderTarget(nullptr);
-#else
-		IDirect3DSurface* pExistingRenderTarget = CxbxGetCurrentRenderTarget();
-		if (pExistingRenderTarget) {
-			(void)CxbxSetRenderTarget(renderTarget);
-			ImGui_ImplDX9_RenderDrawData(drawData);
-			(void)CxbxSetRenderTarget(pExistingRenderTarget);
-			pExistingRenderTarget->Release();
-		}
-#endif
-	}
-}
-
-
 /* Unused :
 static xbox::dword_xt                  *g_Xbox_D3DDevice; // TODO: This should be a D3DDevice structure
 */
