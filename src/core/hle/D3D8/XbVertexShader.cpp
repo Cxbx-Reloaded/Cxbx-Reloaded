0354040fa9bd88878ca4c130b570d1b29ef9831e
medievil1 <38597905+medievil1@users.noreply.github.com>
2026-04-18 11:17:02 -0400

    fix compile error
    correct the #include path for VertexShaderCache.h and shader.h

--- src/core/hle/D3D8/XbVertexShader.cpp
+++ src/core/hle/D3D8/XbVertexShader.cpp
@@ -34,8 +34,8 @@
 #include "core\kernel\support\Emu.h"
 #include "core\hle\D3D8\Rendering\RenderGlobals.h" // For g_Xbox_VertexShader_Handle
 #include "core\hle\D3D8\Rendering\RenderStates.h" // For XboxRenderStateConverter
-#include "core\hle\D3D8\Rendering\VertexShaderCache.h" // For g_VertexShaderCache
-#include "core\hle\D3D8\Rendering\Shader.h" // For g_ShaderSources
+#include "core\hle\D3D8\Rendering\Shaders\VertexShaderCache.h" // For g_VertexShaderCache
+#include "core\hle\D3D8\Rendering\Shaders\Shader.h" // For g_ShaderSources
 #include "core\hle\D3D8\XbVertexBuffer.h" // For CxbxImpl_SetVertexData4f
 #include "core\hle\D3D8\XbVertexShader.h"
 #include "core\hle\D3D8\XbD3D8Logging.h" // For DEBUG_D3DRESULT
